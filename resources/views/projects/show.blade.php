1ca479d6bf16b52a88fb2e88a8645d56c72daa9d
Tarik Dedic <tarikdedic95@gmail.com>
2018-11-06 00:32:11 +0100

    Izmjena u show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.app')
 
 @section ('content')
+<div class="row">
 	<div class="col-md-8 offset-1 mt-5">
 		<a class="btn btn-danger mb-4" href="/control">Nazad</a>
-		
+
 		<h4>Naziv projekta: {{ $project->projekat }} (<a href="{{ $project->link }}" target="_blank">Link projekta</a>)</h4>
+		<hr>
 		<h4>Email klijenta: {{ $project->email_klijenta }} </h4>
-		<p class="lead"> {{ $project->pristupni_podaci }} </p>
+		<hr>
+		<p class="lead"> {!! $project->pristupni_podaci !!} </p>
 		
 	</div>
+	<div class="col-md-2">
+		test
+	</div>
+</div>
 @endsection
